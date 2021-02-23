9958d3c06d055bd4e00fcfc00039c4e4552d2d3c
Brendon Costa <brendoncosta93@gmail.com>
2021-02-23 10:43:04 -0300

    feat(modal): adjustment modal

--- projects/ui/src/lib/layout/modal/modal.component.ts
+++ projects/ui/src/lib/layout/modal/modal.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ho-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.css']
+  styleUrls: ['./modal.component.scss']
 })
 export class ModalComponent implements OnInit {
 
